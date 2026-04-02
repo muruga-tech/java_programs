13e5b8690328f41ec2f4354516aeffe19e376fea
muruga-tech <murugabharathi413@gmail.com>
2026-04-02 14:14:39 +0530

    Add files via upload

---
+++ Selection_sort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+    public static void selectionsort(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            int min_index=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_index]){
+                    min_index=j;
+                }
+                    int temp=arr[i];
+                    arr[i]=arr[min_index];
+                    arr[min_index]=temp;
+            }    
+            
+        }
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    
+}
+}
